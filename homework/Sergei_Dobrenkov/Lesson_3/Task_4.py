c6b9aeec8093498f9d424fca64f58325f39fcb84
Sergei Dobrenkov <dobrenkovsergei@mail.ru>
2025-04-07 09:30:07 +0200

    Homework_3.2

--- homework/Sergei_Dobrenkov/Lesson_3/Task_4.py
+++ homework/Sergei_Dobrenkov/Lesson_3/Task_4.py
@@ -1,11 +1,11 @@
-#Даны катеты прямоугольного треугольника.
-#Найти его гипотенузу и площадь
+# Даны катеты прямоугольного треугольника.
+# Найти его гипотенузу и площадь
 
 import math
 
 leg_a = 3
 leg_b = 4
 hypotenuse = math.sqrt((leg_a * leg_a) + (leg_b * leg_b))
-print(hypotenuse )
+print(hypotenuse)
 square = (leg_a * leg_b / 2)
 print(square)
